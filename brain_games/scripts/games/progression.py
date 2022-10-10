d22e8c8746489639e018f1128656e4a143be3dc5
ni.vasilev <mailto:ni.vasilev@andersenlab.com>
2022-10-10 14:04:09 +0300

    fix

--- brain_games/scripts/games/progression.py
+++ brain_games/scripts/games/progression.py
@@ -3,6 +3,7 @@ import random
 
 
 def main():
+    print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
     print('What number is missing in the progression?')
