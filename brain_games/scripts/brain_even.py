4e95577e184a50b86c59f4589620a31bd369e225
ni.vasilev <mailto:ni.vasilev@andersenlab.com>
2022-10-10 12:19:59 +0300

    first game made

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import brain_games.scripts.games
+
+
+def main():
+    brain_games.scripts.games.is_even()
+
+
+if __name__ == '__main__':
+    main()
---
+++ brain_games/scripts/games.py
@@ -0,0 +1,22 @@
+import prompt
+import random
+
+
+def is_even():
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    right_answer_count = 0
+    while (right_answer_count != 3):
+        if (ask(random.randint(0, 22))):
+            right_answer_count += 1
+    print(f'Congratulations, {name}!')
+
+
+def ask(number):
+    print(f'Question: {number}')
+    is_even_number = 'yes' if number % 2 == 0 else 'no'
+    if (prompt.string('Your answer: ') == is_even_number):
+        print('Correct!')
+        return True
+    return False
