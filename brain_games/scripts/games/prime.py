c1d89daccafaf2138619a26b2cf1c15cabdf5599
ni.vasilev <mailto:ni.vasilev@andersenlab.com>
2022-10-10 14:38:15 +0300

    fix

--- brain_games/scripts/games/prime.py
+++ brain_games/scripts/games/prime.py
@@ -6,7 +6,7 @@ def main():
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
-    print('Answer "yes" if the number is even, otherwise answer "no".')
+    print('Answer "yes" if given number is prime. Otherwise answer "no".')
     right_answer_count = 0
     while (right_answer_count != 3):
         if (ask(random.randint(0, 22))):
