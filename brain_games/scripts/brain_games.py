f169645aa74c2c46fb2c2db581b2adf6d0c121bc
ni.vasilev <mailto:ni.vasilev@andersenlab.com>
2022-10-10 11:44:53 +0300

    Добавлен prompt

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
+import brain_games.scripts.cli
 
 def main():
-    print('Welcome to the Brain Games!')
-
+    brain_games.scripts.cli.welcome_user()
 
 if __name__ == '__main__':
     main()
