2daf503af8276afeb1d2a1af8c6c093c9ac7338f
ni.vasilev <mailto:ni.vasilev@andersenlab.com>
2022-10-10 13:10:40 +0300

    added prime

---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import brain_games.scripts.games.prime
+
+
+def main():
+    brain_games.scripts.games.prime.main()
+
+
+if __name__ == '__main__':
+    main()
---
+++ brain_games/scripts/games/prime.py
@@ -0,0 +1,28 @@
+import prompt
+import random
+
+
+def main():
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    right_answer_count = 0
+    while (right_answer_count != 3):
+        if (ask(random.randint(0, 22))):
+            right_answer_count += 1
+    print(f'Congratulations, {name}!')
+
+
+def ask(number):
+    print(f'Question: {number}')
+    is_prime_number = 'yes' if is_prime(number) else 'no'
+    if (prompt.string('Your answer: ') == is_prime_number):
+        print('Correct!')
+        return True
+    return False
+
+def is_prime(n):
+    d = 2
+    while n % d != 0:
+        d += 1
+    return d == n
